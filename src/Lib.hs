cbef3cf29ed845d6e57d87cee08e956b232a1b25
Chris Howe-Jones <chris.howejones@gmail.com>
2017-05-16 07:19:55 +0100

    Complete implementation

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,8 +6,9 @@ module Lib
     , findWord
     , findWordInLine
     , findWords
+    , skew
     ) where
-import Data.List (isInfixOf)
+import Data.List (isInfixOf, transpose)
 import Data.Maybe (catMaybes)
 
 type Grid = [String]
@@ -18,9 +19,26 @@ outputGrid grid = putStrLn (formatGrid grid)
 formatGrid :: Grid -> String
 formatGrid = unlines
 
+getLines :: Grid -> [String]
+getLines grid =
+  let horizontal = grid
+      vertical = transpose grid
+      diagonal1 = diagonalize horizontal
+      diagonal2 = diagonalize (map reverse vertical)
+      lines = horizontal ++ vertical ++ diagonal1 ++ diagonal2
+  in lines ++ (map reverse lines)
+
+diagonalize :: Grid -> Grid
+diagonalize = transpose . skew
+
+skew :: Grid -> Grid
+skew [] = []
+skew (l:ls) = l : skew (map indent ls)
+  where indent line = '_' : line
+
 findWord :: Grid -> String -> Maybe String
 findWord grid word =
-  let lines = grid ++ (map reverse grid)
+  let lines = getLines grid
       found = or $ map (findWordInLine word) lines
   in if found then Just word else Nothing
 
